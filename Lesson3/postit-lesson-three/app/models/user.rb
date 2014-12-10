aabaee84e5b2201f57e93f91f9f9fc05d205e9af
Liam Sean Brady <liamseanbrady@gmail.com>
2014-12-10 23:58:13 +0000

    Remove Lesson3 and Lesson4 versions of app

--- Lesson3/postit-lesson-three/app/models/user.rb
+++
@@ -1,8 +0,0 @@
-class User < ActiveRecord::Base
-  has_many :posts
-  has_many :comments
-  has_secure_password validations: false
-
-  validates :username, presence: true, uniqueness: true
-  validates :password, presence: true, length: {minimum: 8}, on: :create
-end
